0ee47d6d52a5c1dc4e50aee4948a082b3aa640c6
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-24 00:30:10 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'first ingredient')
+    @recipe.ingredients.build(name: 'second ingredient')
   end
 
   def create
+    Recipe.create(recipe_params)
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ]
+    )
   end
 end
